4b26d279ca9f70b227cc94a10b4816cce720ce4a
dghcjbt7tn-spec <dghcjbt7tn@privaterelay.appleid.com>
2026-08-22 22:55:11 +0200

    Enable chat runtime presence badges and notifications

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './styles.css'
 import './chat-polish.css'
+import './chat-runtime.js'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
